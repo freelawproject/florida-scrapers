4761f85bd4ffcabb2cc052119d42b8bd822660eb
Nick Sethi <nicholas.sethi@gmail.com>
2021-06-22 23:40:12 -0700

    add plugins, build searchpage functionality

--- src/lib/initBrowser.ts
+++ src/lib/initBrowser.ts
@@ -1,12 +1,32 @@
-import puppeteer, { Browser } from "puppeteer"
+import { Browser } from "puppeteer"
+import puppeteer from "puppeteer-extra"
+import RecaptchaPlugin from "puppeteer-extra-plugin-recaptcha"
+import StealthPlugin from "puppeteer-extra-plugin-stealth"
 
 let browser: Browser
+// add recaptcha plugin and provide it your 2captcha token (= their apiKey)
+// 2captcha is the builtin solution provider but others would work as well.
+// Please note: You need to add funds to your 2captcha account for this to work
 
 export const initBrowser = async (): Promise<Browser> => {
   if (!browser) {
     try {
+      // enable the stealth plugin
+      puppeteer.use(StealthPlugin())
+      // enable the captcha plugin
+      puppeteer.use(
+        RecaptchaPlugin({
+          provider: {
+            id: "2captcha",
+            token: "XXXXXXXX", // REPLACE WITH OWN 2CAPTCHA API KEY
+          },
+          visualFeedback: true,
+        })
+      )
+
       browser = await puppeteer.launch({
-        headless: true,
+        //@ts-expect-error incorrect types when using puppeteer-extra
+        headless: false,
         args: ["--disable-setuid-sandbox"],
         ignoreHTTPSErrors: true,
       })
--- src/stjohns/scraper.ts
+++ src/stjohns/scraper.ts
@@ -1,4 +1,4 @@
-import { Browser } from "puppeteer"
+import { Browser, ElementHandle, Page } from "puppeteer"
 
 export class StJohnsScraper {
   _url: string
@@ -14,9 +14,97 @@ export class StJohnsScraper {
     console.log(`Navigating to ${this._url}`)
     await page.goto(this._url)
 
-    await page.waitForSelector("body")
+    try {
+      await page.waitForSelector("form.searchform")
 
-    const content = await page.content()
-    console.log(content)
+      // show dropdown
+      // const dropdownToggler = await findDropdownToggle(page)
+      // dropdownToggler.click()
+
+      // select probate and guardianship
+      // await selectRelevantCourts(page)
+
+      // hide dropdown
+      // dropdownToggler.click()
+
+      // click captcha
+      // await clickCaptcha(page)
+
+      // click submit
+      // await clickSubmit(page)
+
+      const form = await page.$("form.searchform")
+
+      // set the input value to be 4,6
+      await form.$$eval("input", (els) => {
+        const newEls = els.map((el) => {
+          // @ts-expect-error no element properties?
+          if (el.name === "courtTypes") {
+            //@ts-expect-error no element properties?
+            el.value = "4,6"
+            //@ts-expect-error no element properties?
+          } else if (el.name === "openedFrom") {
+            //@ts-expect-error no element properties?
+          } else if (el.name === "openedTo") {
+          }
+          return el
+        })
+        return newEls
+      })
+      await clickSubmit(page)
+    } catch (e) {
+      console.log(e)
+    }
   }
+}
+
+const waitFor = (delay: number): Promise<void> => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay)
+  })
+}
+
+const findDropdownToggle = async (page: Page): Promise<ElementHandle<Element>> => {
+  const el = await page.$("button#courTypesButton")
+  return el
+}
+
+const selectRelevantCourts = async (page: Page): Promise<void> => {
+  await waitFor(500)
+  // find the select dropdown
+  const dropdown = await page.$("div#courTypesMultiSelect")
+
+  // find the ul options
+  const options = await dropdown.$("ul.dropdown-menu")
+
+  // find the selectAll option and click it to reset selections
+  const selectAllOption = await options.$('a > label.checkbox > input[value="multiselect-all"]')
+  await selectAllOption.click()
+
+  await waitFor(500)
+  // select probate --> option "4"
+  const probateOption = await options.$('a > label.checkbox > input[value="4"]')
+  probateOption.click()
+
+  await waitFor(500)
+  // select guardianship --> option "6"
+  const guardianshipOption = await options.$('a > label.checkbox > input[value="6"]')
+  console.log("got guardianship option")
+  guardianshipOption.click()
+}
+
+const clickCaptcha = async (page: Page): Promise<void> => {
+  const captchaSpan = await page.$("span.recaptcha-checkbox")
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2600)
+  })
+  await captchaSpan.click()
+}
+
+const clickSubmit = async (page: Page): Promise<void> => {
+  const submitButton = await page.$("button#searchButton")
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1600)
+  })
+  await submitButton.click()
 }
