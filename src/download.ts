99072d7ac172e30e5e7e0919d2e3cca921f58aee
Nick Sethi <nicholas.sethi@gmail.com>
2021-07-03 17:10:53 -0700

    download files

--- src/download.ts
+++ src/download.ts
@@ -1,5 +1,14 @@
 import { initBrowser } from "./lib/browser"
 import { StJohnsScraper } from "./stjohns/scraper"
+declare global {
+  interface Window {
+    username: string
+    password: string
+    caseNo?: string
+    startDate?: string
+    endDate?: string
+  }
+}
 
 const scrapeStJohns = async (): Promise<void> => {
   const browser = await initBrowser()
